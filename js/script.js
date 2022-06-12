d7630ac31a173fa3e6f8d347f12e3046c1c31c21
Mef <example@gmail.com>
2022-06-12 22:28:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+// !Простое общение с пользователем
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', "18");
+// console.log(answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", " ");
+// answers[1] = prompt("Как ваша фамилия?", " ");
+// answers[2] = prompt("Сколько вам лет?", " ");
+
+// document.write(answers);
+
+// !Интерполяция
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+
+// !Операторы
+
+// console.log('arr' + '- object');
+// console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(112%5);
+
+// console.log(2*4 == '8');
+
+const isChecked = false,
+      isClosed = false;
+
+console.log(isChecked || !isClosed);
+
+console.log(2 + 2 * 2 === 8);
