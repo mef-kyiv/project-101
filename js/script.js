7aa1e0c1b3ac838b5b3d222861087f0abb0bec43
Mef <example@gmail.com>
2022-06-14 23:30:19 +0300

    terms

--- js/script.js
+++ js/script.js
@@ -49,3 +49,40 @@
 // console.log(isChecked || !isClosed);
 
 // console.log(2 + 2 * 2 === 8);
+
+// Условия
+
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log ('Too much');
+// } else { 
+//     console.log('Ok!');
+// }
+
+// (num == 50) ? console.log('Ok!') : console.log('Error');
+
+// const num2 = 100;
+
+// switch (num2) {
+//     case 49: 
+//              console.log('Error');
+//     break;
+//     case 100: 
+//              console.log('Too much');
+//     break;
+//     case 50: 
+//              console.log('Ok!');
+//     break;
+//     default: 
+//              console.log('Not Ok!');
+// }
+                
