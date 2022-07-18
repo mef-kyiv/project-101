f99237e4a34473497eeae5abbc447b6240e12b60
Mef <example@gmail.com>
2022-07-18 23:11:14 +0300

    Callback, Objects, Array theme

--- js/script.js
+++ js/script.js
@@ -150,20 +150,118 @@
 //     return a + b;
 // }
 
-// Методы и свойства строк и чисел
+// // Методы и свойства строк и чисел
 
-// const str = "test";
-// const arr = [1, 2, 3];
-// console.log(str.toUpperCase());
+// // const str = "test";
+// // const arr = [1, 2, 3];
+// // console.log(str.toUpperCase());
 
-// const fruit = "Some fruit";
-// console.log(fruit.indexOf('fruit'));
+// // const fruit = "Some fruit";
+// // console.log(fruit.indexOf('fruit'));
 
-// const logg = 'Hello world';
-// console.log(logg.slice(6, 7));
+// // const logg = 'Hello world';
+// // console.log(logg.slice(6, 7));
 
-// const num = 12.2;
-// console.log(Math.round(num));
+// // const num = 12.2;
+// // console.log(Math.round(num));
 
-// const test = "12.2px";
-// console.log(parseFloat(test));
+// // const test = "12.2px";
+// // console.log(parseFloat(test));
+
+// //CALLBACK ФУНКЦИИ
+
+// // function first() {
+// //     // do something
+// //     setTimeout(function() {
+// //         console.log(1);
+// //     }, 500);
+// // }
+
+// // function second() {
+// //     console.log(2);
+// // }
+
+// // first();
+// // second();
+
+// // function learnJS(lang, callback) {
+// //     console.log(`Я учу: ${lang}`);
+// //     callback();
+// // }
+
+// // function done() {
+// //     console.log('Я прошел этот урок');
+// // }
+
+// // learnJS('JavaScript', done);
+
+// // Объекты деструктуризация объектов
+
+// const options = {
+//     name: `test`,
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red',
+//     },
+//     makeTest: function() {
+//         console.log("Test");
+//     }
+// };
+
+// options.makeTest();
+
+// const {border, bg} = options.colors;
+// console.log(border);
+
+// // console.log(Object.keys(options).length);
+
+// // console.log(options[`colors`][`border`]);
+
+// // delete options.name;
+
+// // console.log(options);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     counter++;
+//     }
+// }
+
+// console.log(counter);
+
+// Масивы и псевдомасивы
+
+const arr = [1, 2, 3, 6, 8];
+
+// arr.forEach(function(item, i, arr){
+//     console.log(`${i}: ${item} внутри масива ${arr}`);
+// });
+
+// console.log(arr.length);
+// // arr.pop();
+// // arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products.join('; '));
